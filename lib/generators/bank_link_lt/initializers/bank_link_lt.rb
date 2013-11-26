4a737590caf1fbe968f952112714d991355a97b1
Saulius Morkevicius <saulius.m@gmail.com>
2013-11-26 14:40:19 +0200

    initializer update

--- lib/generators/bank_link_lt/initializers/bank_link_lt.rb
+++ lib/generators/bank_link_lt/initializers/bank_link_lt.rb
@@ -14,10 +14,10 @@ BankLinkLt::Swedbank.account_payment_gateway_url = ''
 BankLinkLt::Swedbank.test_account_payment_gateway_url = 'http://test-pay.com/bank-link/ibpay/swedbank'
 
 BankLinkLt::Swedbank.account_bank_certificate = ''
-BankLinkLt::Swedbank.test_account_bank_certificate = File.read("#{Rails.root}/certificates/")
+#BankLinkLt::Swedbank.test_account_bank_certificate = File.read("#{Rails.root}/certificates/")
 
 BankLinkLt::Swedbank.account_private_key = ''
-BankLinkLt::Swedbank.test_account_private_key =  File.read("#{Rails.root}/certificates/")
+#BankLinkLt::Swedbank.test_account_private_key =  File.read("#{Rails.root}/certificates/")
 
 #-------------------------------------------------------------------
 
@@ -38,9 +38,9 @@ BankLinkLt::Seb.test_account_sender_id = ''
 BankLinkLt::Seb.account_payment_gateway_url = 'https://ebankas.seb.lt/cgi-bin/vbint.sh/vbnet.w'
 BankLinkLt::Seb.test_account_payment_gateway_url = 'http://test-pay.com/bank-link/ibpay/seb'
 
-BankLinkLt::Seb.account_bank_certificate = File.read("#{Rails.root}/certificates/")
-BankLinkLt::Seb.test_account_bank_certificate = File.read("#{Rails.root}/certificates/")
+#BankLinkLt::Seb.account_bank_certificate = File.read("#{Rails.root}/certificates/")
+#BankLinkLt::Seb.test_account_bank_certificate = File.read("#{Rails.root}/certificates/")
 
-BankLinkLt::Seb.account_private_key = ''
-BankLinkLt::Seb.test_account_private_key =  File.read("#{Rails.root}/certificates/")
+#BankLinkLt::Seb.account_private_key = ''
+#BankLinkLt::Seb.test_account_private_key =  File.read("#{Rails.root}/certificates/")
 
