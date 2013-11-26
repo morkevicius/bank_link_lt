0f869810a1389fca533810ab0cabafc548740ca6
Saulius Morkevicius <saulius.m@gmail.com>
2013-11-26 15:04:18 +0200

    initializer update

--- lib/generators/bank_link_lt/install_generator.rb
+++ lib/generators/bank_link_lt/install_generator.rb
@@ -7,20 +7,26 @@ module BankLinkLt
 
 
     INITIALIZER_NAME = 'bank_link_lt.rb'
-    CERTIFICATES_PATH = '/certificates/'
+    CERTIFICATES_PATH = 'certificates/'
 
 
     def copy_initializer
       unless File.exist?(Rails.root + 'config/initializers/' + INITIALIZER_NAME) || File.symlink?(Rails.root + 'config/initializers/' + INITIALIZER_NAME)
+        puts 'Copying initializer'
         copy_file 'initializers/' + INITIALIZER_NAME, Rails.root + 'config/initializers/' + INITIALIZER_NAME
+      else
+        puts 'Initializer already exist'
       end
     end
 
     def create_certificates_directory
 
       unless File.directory?(Rails.root + CERTIFICATES_PATH)|| File.symlink?(Rails.root + CERTIFICATES_PATH)
-        Dir.mkdir Rails.root + CERTIFICATES_PATH
-        system "chmod  700 #{Rails.root + CERTIFICATES_PATH}"
+        puts "Creating directory for certificates at #{Rails.root} #{CERTIFICATES_PATH} "
+        Dir.mkdir "#{Rails.root} #{CERTIFICATES_PATH}"
+        #system "chmod  700 #{Rails.root + CERTIFICATES_PATH}"
+      else
+        puts "Directory for certificates at #{Rails.root} #{CERTIFICATES_PATH} already exists"
       end
 
     end
