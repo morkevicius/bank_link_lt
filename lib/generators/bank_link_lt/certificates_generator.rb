569dc0e9d4872fa2ff441d4d2a5a5363bd0f5389
Saulius Morkevicius <saulius.m@gmail.com>
2013-11-26 15:44:12 +0200

    initializer update

--- lib/generators/bank_link_lt/certificates_generator.rb
+++ lib/generators/bank_link_lt/certificates_generator.rb
@@ -9,7 +9,7 @@ module BankLinkLt
 
       source_root File.expand_path(File.join(File.dirname(__FILE__)))
 
-      class_option :certificates_subject, :type => :string, :default => '/C=LT/L=Vilnius/O=JUSU UAB/OU=BankLink/CN=www.jusu_www.lt/emailAddress=info@jusu_www.lt/'
+      class_option :certificates_subject, :type => :string, :default => '/C=country_code/L=city/O=company_name/OU=BankLink/CN=www.your_www.lt/emailAddress=your_company_email/'
       class_option :certificates_path, :type => :string, :default => CERTIFICATES_PATH
 
 
@@ -29,7 +29,7 @@ module BankLinkLt
         puts "Will generate certificates for #{BANKS.join(', ')} \n"
         BANKS.each do |bank|
           puts "options for #{bank}: #{options[:certificates_subject]}"
-          `cd #{CERTIFICATES_FULL_PATH} &&
+          `cd #{Rails.root + options[:certificates_path]} &&
            openssl req -new -nodes -nodes -days 720 -x509 -newkey rsa:1024 -out certificate_for_#{bank}.crt -keyout private_key_for_#{bank}.pem -subj "#{options[:certificates_subject]}" &&
            && openssl x509 -x509toreq -in certificate_for_#{bank}.crt -out #{bank}_certificate_request.csr -signkey private_key_for_#{bank}.pem`
 
