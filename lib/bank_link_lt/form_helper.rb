ef587b343f50ef9e38b15efdc6dfba2964b5f7e7
Saulius Morkevicius <saulius.m@gmail.com>
2013-11-28 11:57:30 +0200

    params to hash

--- lib/bank_link_lt/form_helper.rb
+++ lib/bank_link_lt/form_helper.rb
@@ -1,6 +1,6 @@
 module BankLinkLt::FormHelper
-  def payment_form(options = {:request_params_hash => request_params_hash, :payment_url => payment_url, :submit_name => submit_name})
-    content_tag :form, :action => url, :method => 'POST', :target => '_blank' do
+  def payment_form(options = {:request_params_hash => request_params_hash, :payment_url => payment_url, :submit_name => submit_name, :id => id})
+    content_tag :form, :action => payment_url, :method => 'POST', :id => id, :target => '_blank' do
       request_params_hash.each do |key, value|
         concat(tag(:input, :name => key, :value => value, :type => :hidden))
       end
